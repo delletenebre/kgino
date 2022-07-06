ce5d2aef535b2e5a49aedc0682666b142e5873c3
snezhinskikh <nezhinskikh.s@infocom.kg>
2022-07-06 18:16:51 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:kgino/app.dart';
-import 'package:kgino/models/favorite_model.dart';
-import 'package:kgino/models/viewed_episode_model.dart';
-import 'package:kgino/models/viewed_show_model.dart';
 
-import 'controllers/controllers.dart';
+import 'app.dart';
 
-void main() async {
-  await Hive.initFlutter();
-
-  Hive.registerAdapter<FavoriteModel>(FavoriteModelAdapter());
-  Hive.registerAdapter<ViewedShowModel>(ViewedShowModelAdapter());
-  Hive.registerAdapter<ViewedEpisodeModel>(ViewedEpisodeModelAdapter());
-
-  /// контроллер избранного
-  await Hive.openBox<FavoriteModel>(FavoritesController.storageName);
-  Get.put(FavoritesController());
-
-  /// контроллер просмотренного
-  await Hive.openBox<ViewedShowModel>(ViewedController.storageName);
-  Get.put(ViewedController());
-
+void main() {
   runApp(const App());
 }
