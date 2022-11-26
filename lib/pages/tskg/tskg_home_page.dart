fef14350c1901358fac8705116fd9f8afab989e6
Sergei Nezhinskikh <delletenebre@gmail.com>
2022-11-26 13:06:05 +0600

    add tskg models

--- lib/pages/tskg/tskg_home_page.dart
+++ lib/pages/tskg/tskg_home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../controllers/tskg/tskg_show_details_controller.dart';
+import '../../models/tskg/tskg_show.dart';
+import '../../ui/pages/tskg/tskg_home_page_list_view.dart';
 
 class TskgHomePage extends StatelessWidget {
   const TskgHomePage({
@@ -7,8 +12,43 @@ class TskgHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('В разработке'),
+    return BlocProvider(
+      create: (context) => TskgShowDetailsController(),
+      child: Column(
+        children: [
+          /// детали фильма, при фокусе на каком-либо фильме
+          /// или первый популярный фильм, если ничего не выбрано
+          Expanded(
+            child: BlocBuilder<TskgShowDetailsController, RequestState<TskgShow>>(
+              builder: (context, state) {
+                if (state.isSuccess) {
+                  final show = state.data;
+
+                  return SizedBox();
+                  // if (movie.showPlayButton) {
+                  //   return OckgMoviePopular(
+                  //     movie: movie,
+                  //   );
+                  // } else {
+                  //   return OckgMovieDetais(
+                  //     movie: movie,
+                  //   );
+                  // }
+                  
+                }
+
+                return const SizedBox();
+              }
+            ),
+          ),
+
+          /// список фильмов внизу страницы
+          const SizedBox(
+            height: 252.0,
+            child: TskgHomePageListView(),
+          ),
+        ],
+      ),
     );
   }
 }
