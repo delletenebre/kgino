218d4a9e28e9bfa2acfe71246abc0e87a1afde66
snezhinskikh <nezhinskikh.s@infocom.kg>
2022-12-12 10:51:45 +0600

    add firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -8,6 +12,7 @@ import 'api/tskg_api_provider.dart';
 import 'api/wcam_api_provider.dart';
 import 'app.dart';
 import 'controllers/seen_items_controller.dart';
+import 'firebase_options.dart';
 import 'models/episode_item.dart';
 import 'models/movie_item.dart';
 import 'resources/krs_storage.dart';
@@ -29,6 +34,23 @@ Future<void> main() async {
 
   /// инициализируем локальное хранилище
   final sharedStorage = await SharedPreferences.getInstance();
+
+  /// инициализируем firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  /// настраиваем crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  
+  /// pass all uncaught asynchronous errors that aren't handled by the Flutter
+  /// framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   /// регистрируем [KrsStorage] как singleton
   GetIt.instance.registerSingleton<KrsStorage>(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
