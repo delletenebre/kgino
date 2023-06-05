7f31cfef62908a150fa934c4658ded2cccd6b11d
Sergei Nezhinskikh <delletenebre@gmail.com>
2023-06-05 21:15:45 +0600

    add mouse control

--- lib/ui/video_player/video_player_controls_overlay.dart
+++ lib/ui/video_player/video_player_controls_overlay.dart
@@ -103,209 +103,250 @@ class _VideoPlayerControlsOverlayState extends State<VideoPlayerControlsOverlay>
       _progressBarFocusNode.requestFocus();
     }
 
-    return Focus(
-      autofocus: true,
-      skipTraversal: true,
-      
-      onKey: (node, event) {
-
-        if (!widget.isVisible) {
-          /// ^ если оверлей не был виден пользователю
-          
-          if (event.isKeyPressed(LogicalKeyboardKey.select) || event.isKeyPressed(LogicalKeyboardKey.enter)) {
-            /// ^ если был нажат Enter
-            
-            /// вызываем обработчик нажатия на play/pause
-            widget.onPlayPause.call();
-            
-            /// ставим фокус на ProgressBar
-            _progressBarFocusNode.requestFocus();
-            
-            return KeyEventResult.handled;
-          }
-        }
+    return GestureDetector(
+      onTap: () {
+        /// вызываем обработчик нажатия на play/pause
+        widget.onPlayPause.call();
+        
+        /// ставим фокус на ProgressBar
+        _progressBarFocusNode.requestFocus();
 
         widget.onShowOverlay();
-
-        return KeyEventResult.ignored;
       },
+      child: Focus(
+        autofocus: true,
+        skipTraversal: true,
+        
+        onKey: (node, event) {
+          if (!widget.isVisible) {
+            /// ^ если оверлей не был виден пользователю
+            
+            if (event.isKeyPressed(LogicalKeyboardKey.select) || event.isKeyPressed(LogicalKeyboardKey.enter)) {
+              /// ^ если был нажат Enter
+              
+              /// вызываем обработчик нажатия на play/pause
+              widget.onPlayPause.call();
+              
+              /// ставим фокус на ProgressBar
+              _progressBarFocusNode.requestFocus();
+              
+              return KeyEventResult.handled;
+            }
+          }
 
-      child: AnimatedOpacity(
-        duration: KrsTheme.animationDuration,
-        opacity: widget.isVisible ? 1.0 : 0.0,
-        child: Stack(
-          children: [
-            /// название видео
-            Positioned(
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: AnimatedContainer(
-                duration: KrsTheme.animationDuration,
-                height: MediaQuery.of(context).size.height * 0.5,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                      widget.isVisible ? theme.colorScheme.surface : Colors.transparent,
-                      Colors.transparent,
-                    ],
+          if (event.isKeyPressed(LogicalKeyboardKey.escape) || event.isKeyPressed(LogicalKeyboardKey.backspace)) {
+            if (widget.playerController == null || !widget.playerController!.value.isPlaying) {
+              Navigator.of(context).pop();
+            } else {
+              widget.playerController?.pause();
+            }
+          }
+
+          if (widget.playerController != null) {
+
+            if (event.isKeyPressed(LogicalKeyboardKey.space)) {
+              if (widget.playerController!.value.isPlaying) {
+                widget.playerController?.pause();
+              } else {
+                widget.playerController?.play();
+              }
+            }
+
+            if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+              widget.playerController!.position.then((position) {
+                widget.playerController?.seekTo(Duration(seconds: position!.inSeconds + 10));
+              });
+            }
+
+            if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+              widget.playerController!.position.then((position) {
+                widget.playerController?.seekTo(Duration(seconds: position!.inSeconds - 10));
+              });
+            }
+          }
+
+          widget.onShowOverlay();
+
+          return KeyEventResult.ignored;
+        },
+
+        child: AnimatedOpacity(
+          duration: KrsTheme.animationDuration,
+          opacity: widget.isVisible ? 1.0 : 0.0,
+          child: Stack(
+            children: [
+              /// название видео
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: AnimatedContainer(
+                  duration: KrsTheme.animationDuration,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        widget.isVisible ? theme.colorScheme.surface : Colors.transparent,
+                        Colors.transparent,
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
 
-            /// название видео
-            AnimatedPositioned(
-              duration: KrsTheme.animationDuration,
-              top: widget.isVisible ? 48.0 : 0.0,
-              left: 48.0,
-              right: 48.0,
-              child: Column(
-                children: [
-                  /// название видео
-                  _TitleText(widget.titleText,
-                    fontSize: 26.0,
-                  ),
-
-                  /// дополнительная информация о видео
-                  if (widget.subtitleText.isNotEmpty) Padding(
-                    padding: const EdgeInsets.only(top: 12.0),
-                    child: _TitleText(widget.subtitleText,
-                      fontSize: 18.0,
+              /// название видео
+              AnimatedPositioned(
+                duration: KrsTheme.animationDuration,
+                top: widget.isVisible ? 48.0 : 0.0,
+                left: 48.0,
+                right: 48.0,
+                child: Column(
+                  children: [
+                    /// название видео
+                    _TitleText(widget.titleText,
+                      fontSize: 26.0,
                     ),
-                  ),
-                ],
-              ),
-            ),
 
-            /// если видео загружается
-            if (isLoading) const LoadingIndicator(
-              size: 64.0,
-            ),
-
-            /// остановить/продолжить воспроизведение
-            if (!isLoading) Center(
-              child: ValueListenableBuilder(
-                valueListenable: widget.playerController!,
-                builder: (context, VideoPlayerValue video, child) {
-                  return PlayPauseButton(
-                    isPlaying: video.isPlaying,
-                  );
-                },
-              ),
-            ),
-
-            /// нижняя панель
-            if (!isLoading) AnimatedPositioned(
-              duration: KrsTheme.animationDuration,
-              bottom: widget.isVisible ? 32.0 : 0.0,
-              left: 32.0,
-              right: 32.0,
-              child: Column(
-                children: [
-                  if (widget.playerController!.value.duration.inSeconds > 0) Padding(
-                    padding: const EdgeInsets.only(bottom: 12.0),
-                    child: VideoPlayerProgressBar(
-                      focusNode: _progressBarFocusNode,
-                      playerController: widget.playerController!,
-                      onEnter: () {
-                        widget.onPlayPause();
-                      },
-                      onSeek: (duration) {
-                        widget.onSeek.call(duration);
-                      },
+                    /// дополнительная информация о видео
+                    if (widget.subtitleText.isNotEmpty) Padding(
+                      padding: const EdgeInsets.only(top: 12.0),
+                      child: _TitleText(widget.subtitleText,
+                        fontSize: 18.0,
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+              ),
 
-                  Row(
-                    children: [
-                      /// кнопка включения субтитров
-                      if (hasSubtitles && !widget.subtitlesEnabled) VideoPlayerControlsButton(
-                        
-                        onPressed: () {
-                          /// вызываем пользовательский обработчик включения
-                          /// субтитров
-                          widget.onSubtitleToggle(true);
+              /// если видео загружается
+              if (isLoading) const LoadingIndicator(
+                size: 64.0,
+              ),
+
+              /// остановить/продолжить воспроизведение
+              if (!isLoading) Center(
+                child: ValueListenableBuilder(
+                  valueListenable: widget.playerController!,
+                  builder: (context, VideoPlayerValue video, child) {
+                    return PlayPauseButton(
+                      isPlaying: video.isPlaying,
+                    );
+                  },
+                ),
+              ),
+
+              /// нижняя панель
+              if (!isLoading) AnimatedPositioned(
+                duration: KrsTheme.animationDuration,
+                bottom: widget.isVisible ? 32.0 : 0.0,
+                left: 32.0,
+                right: 32.0,
+                child: Column(
+                  children: [
+                    if (widget.playerController!.value.duration.inSeconds > 0) Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: VideoPlayerProgressBar(
+                        focusNode: _progressBarFocusNode,
+                        playerController: widget.playerController!,
+                        onEnter: () {
+                          widget.onPlayPause();
+                        },
+                        onSeek: (duration) {
+                          widget.onSeek.call(duration);
+                        },
+                      ),
+                    ),
+
+                    Row(
+                      children: [
+                        /// кнопка включения субтитров
+                        if (hasSubtitles && !widget.subtitlesEnabled) VideoPlayerControlsButton(
                           
-                        },
-                        icon: const Icon(Icons.subtitles),
-                        child: Text(locale.enableSubtitles),
-                        
-                      ),
-
-                      /// кнопка выключения субтитров
-                      if (hasSubtitles && widget.subtitlesEnabled) VideoPlayerControlsButton(
-                        
-                        onPressed: () {
-                          /// вызываем пользовательский обработчик выключения
-                          /// субтитров
-                          widget.onSubtitleToggle.call(false);
-                        },
-                        icon: const Icon(Icons.subtitles_off),
-                        child: Text(locale.disableSubtitles),
-                        
-                      ),
-
-                      /// кнопка смены качества видео
-                      if (widget.onChangeQuality != null) VideoPlayerControlsButton(
-                        
-                        onPressed: () async {
-                          /// вызываем пользовательский обработчик выбора
-                          /// качества видео
-                          widget.onChangeQuality?.call();
-                        },
-                        icon: const Icon(Icons.video_settings_outlined),
-                        child: widget.quality == 0
-                          ? Text('Качество видео')
-                          : Text('${widget.quality}'),
-                        
-                      ),
-
-                      const Expanded(
-                        child: SizedBox(),
-                      ),
-
-                      /// кнопка предыдущего видео
-                      if (widget.onSkipPrevious != null) VideoPlayerControlsButton(
-                        
-                        onPressed: () {
-                          /// вызываем пользовательский обработчик запроса
-                          /// предыдущего видео
-                          widget.onSkipPrevious?.call();
-                        },
-
-                        child: Icon(Icons.skip_previous,
-                          semanticLabel: locale.previousEpisode,
+                          onPressed: () {
+                            /// вызываем пользовательский обработчик включения
+                            /// субтитров
+                            widget.onSubtitleToggle(true);
+                            
+                          },
+                          icon: const Icon(Icons.subtitles),
+                          child: Text(locale.enableSubtitles),
+                          
                         ),
-                        
-                      ),
 
-                      /// кнопка следующего видео
-                      if (widget.onSkipNext != null) Padding(
-                        padding: const EdgeInsets.only(left: 12.0),
-                        child: VideoPlayerControlsButton(
+                        /// кнопка выключения субтитров
+                        if (hasSubtitles && widget.subtitlesEnabled) VideoPlayerControlsButton(
+                          
+                          onPressed: () {
+                            /// вызываем пользовательский обработчик выключения
+                            /// субтитров
+                            widget.onSubtitleToggle.call(false);
+                          },
+                          icon: const Icon(Icons.subtitles_off),
+                          child: Text(locale.disableSubtitles),
+                          
+                        ),
+
+                        /// кнопка смены качества видео
+                        if (widget.onChangeQuality != null) VideoPlayerControlsButton(
+                          
+                          onPressed: () async {
+                            /// вызываем пользовательский обработчик выбора
+                            /// качества видео
+                            widget.onChangeQuality?.call();
+                          },
+                          icon: const Icon(Icons.video_settings_outlined),
+                          child: widget.quality == 0
+                            ? Text('Качество видео')
+                            : Text('${widget.quality}'),
+                          
+                        ),
+
+                        const Expanded(
+                          child: SizedBox(),
+                        ),
+
+                        /// кнопка предыдущего видео
+                        if (widget.onSkipPrevious != null) VideoPlayerControlsButton(
+                          
                           onPressed: () {
                             /// вызываем пользовательский обработчик запроса
-                            /// следующего видео
-                            widget.onSkipNext?.call();
+                            /// предыдущего видео
+                            widget.onSkipPrevious?.call();
                           },
 
-                          child: Icon(Icons.skip_next,
-                            semanticLabel: locale.nextEpisode,
+                          child: Icon(Icons.skip_previous,
+                            semanticLabel: locale.previousEpisode,
                           ),
                           
                         ),
-                      ),
 
-                    ],
-                  ),
-                  
-                ],
+                        /// кнопка следующего видео
+                        if (widget.onSkipNext != null) Padding(
+                          padding: const EdgeInsets.only(left: 12.0),
+                          child: VideoPlayerControlsButton(
+                            onPressed: () {
+                              /// вызываем пользовательский обработчик запроса
+                              /// следующего видео
+                              widget.onSkipNext?.call();
+                            },
+
+                            child: Icon(Icons.skip_next,
+                              semanticLabel: locale.nextEpisode,
+                            ),
+                            
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
