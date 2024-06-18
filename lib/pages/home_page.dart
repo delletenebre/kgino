cbc88051f9ce9c2aab1c7f3c46722ea2ec827174
Sergei Nezhinskikh <delletenebre@gmail.com>
2024-06-18 12:27:53 +0600

    clean up

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../providers/active_horizontal_list_provider.dart';
